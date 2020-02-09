e09ebbf3831b45f9ea1e3fa287f778fa5df159c5
jehouha <jehouha@knights.ucf.edu>
2020-02-08 19:59:27 -0500

    v1.2

---
+++ Team_Project_v1.1/ZeldaRemake1-master/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /////////////////////////////////
+    //         Lives Script        //
+    /////////////////////////////////
+
+    // Sets number of lives in Unity
+    public int lives = 1;
+
+    // Set rupee as loot
+    public GameObject lootDrop;
+
+    void lifeCounter()
+    {
+        if (lives <= 0)
+        {
+            Destroy(this.gameObject);
+            GameObject a = Instantiate(lootDrop, transform.position, lootDrop.transform.rotation) as GameObject;
+        }
+    }
+
+    /////////////////////////////////
+    // 2D Collider Triggers Script //
+    /////////////////////////////////
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("enemy hit");
+        if (other.gameObject.CompareTag("player"))
+        {
+            Debug.Log("enemy damage taken");
+            lives--;
+            lifeCounter();
+        }
+    }
+}
